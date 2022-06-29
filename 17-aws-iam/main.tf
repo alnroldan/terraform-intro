6831eb4fb342bb1f3d041485bb4ca4ffaa6f9998
Jairo  Roldan <jairo.roldan@grupobabel.com>
2022-06-29 02:01:18 -0500

    dia 6 update

--- 17-aws-iam/main.tf
+++ 17-aws-iam/main.tf
@@ -1,8 +1,8 @@
 # remove this part
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIA6DF4DESATARQ6VPN"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
 
 
