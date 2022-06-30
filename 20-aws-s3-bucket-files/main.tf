663bc5cdc7036d260dbf5e1d0ebfb0bac056855e
Jairo  Roldan <jairo.roldan@grupobabel.com>
2022-06-29 23:27:28 -0500

    dia 7

--- 20-aws-s3-bucket-files/main.tf
+++ 20-aws-s3-bucket-files/main.tf
@@ -10,3 +10,10 @@ resource "aws_s3_bucket_object" "s3-bucket-files"{
     key = "TemarioTerraform.docx"
     bucket = aws_s3_bucket.s3-bucket-aws.id
 }
+
+resource "aws_s3_object" "s3-bucket-files-2"{
+    content = "./attachment/AVReceiver.jpg"
+    key = "AVReceiver.jpg"
+    bucket = aws_s3_bucket.s3-bucket-aws.id
+
+}
